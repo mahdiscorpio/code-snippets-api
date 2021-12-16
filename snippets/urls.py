f64932575c9e8a29980c418d20381cd3de559d12
Mahdi Behnam <mahdiscorpio3851099@gmail.com>
2021-12-16 22:31:10 +0330

    main   part 1 & 2 of the tutorial done

---
+++ snippets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+app_name = 'snippets'
+
+urlpatterns = [
+    path('', views.snippet_list, name="snippet_list"),
+    path('<int:pk>/', views.snippet_detail, name="snippet_detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    #MY APPs
+    'snippets',
 ]
 
 MIDDLEWARE = [
